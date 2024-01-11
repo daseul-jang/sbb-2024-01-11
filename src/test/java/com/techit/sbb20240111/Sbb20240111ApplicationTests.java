1262602745917b6eeb246db65daa529308ab5228
daseul-jang <devzzang94@gmail.com>
2024-01-11 13:59:31 +0900

    batch test

---
+++ src/test/java/com/techit/sbb20240111/Sbb20240111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techit.sbb20240111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb20240111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
